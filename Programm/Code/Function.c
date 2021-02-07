3d22ac521290ec2381cdc2a413580b7333992498
Kerriea-star <ojared511@gmail.com>
2021-02-07 18:00:20 +0300

    First commit :octocat:

---
+++ Programm/Code/Function.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void loop()
+{
+	for(int i=0;i<=10;i++);
+	{
+		printf("%d",i);
+	}
+}
+int multiply(int x,int y,int z)
+{
+	int multiplicatio;
+	multiplication=x+y+z;
+	return multiplication;
+}
+int main()
+{
+	loop c;
+	printf("%d",multply(10,20,30);)
+}
