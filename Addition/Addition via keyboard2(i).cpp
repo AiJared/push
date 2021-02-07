008aca48ee7f980045f3f3b1f6c30bb3fcf7c1bd
Kerriea-star <ojared511@gmail.com>
2021-02-07 18:15:22 +0300

    Addition

---
+++ Addition/Addition via keyboard2(i).cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>include <>
+int main()
+{
+	int number1, number2, sum;
+	printf("Enter two integer values\n");
+	scanf("%d%d", &number1, &number2);
+	sum=number1+number2;
+	printf("The sum is %d",sum);
+}
